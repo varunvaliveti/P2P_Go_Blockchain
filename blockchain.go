00457614fbb1f740d3196c2e5ebe1d2e24fc84ca
Varun <valivetiavrun@gmail.com>
2024-11-28 21:10:46 -0800

    General framework, will add wallets and other features later

--- blockchain.go
+++ blockchain.go
@@ -1,24 +1,69 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
-	"time"
 )
 
 type Block struct {
-	hash        string
-	timeStamp   time.Time
-	proofOfWork int
-	data        map[string]interface{}
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
 }
 
-type Blockchain struct {
-	genesisBlock Block
-	chain        []Block
-	difficulty   int
+type BlockChain struct {
+	blocks []*Block
 }
 
-func (b Block) calculateHash() {
+func (b *Block) DeriveHash() {
+	dataForHash := [][]byte{b.Data, b.PrevHash}
+	info := bytes.Join(dataForHash, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{
+
+		Data:     []byte(data),
+		PrevHash: prevHash,
+	}
+
+	block.DeriveHash()
+
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	newBlock := CreateBlock(data, prevBlock.Hash)
+
+	chain.blocks = append(chain.blocks, newBlock)
+}
+
+func Genesis() *Block {
+	return CreateBlock("First", nil)
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{
+		blocks: []*Block{Genesis()},
+	}
+}
+
+func main() {
+	chain := InitBlockChain()
+	chain.AddBlock("First")
+	chain.AddBlock("Second")
+
+	for _, block := range chain.blocks {
+		// fmt.Println("Prev hash ", hex.EncodeToString(block.PrevHash))
+		fmt.Println("data in block ", string(block.Data))
+		fmt.Println("Hash: ", hex.EncodeToString(block.Hash))
+		fmt.Println()
+	}
 
 }
